8ad4d2747ec9ea526d1112ab20935447d8142688
Akhilleshgoswami <agiri5375@gmail.com>
2022-08-10 15:12:56 +0530

    wrote withdrawal api for dydx and test it on mainnet

--- services/dydx_client/__init__.py
+++ services/dydx_client/__init__.py
@@ -1,4 +1,8 @@
 from .admin import DydxAdmin
 from .dydx_p_client import DydxPClient
 from .orders import DydxOrder
+<<<<<<< HEAD
 from .withdrawal import DydxWithdrawal
+=======
+from .withdrawal import  DydxWithdrawal
+>>>>>>> 393c1c8 (wrote withdrawal api for dydx and test it on mainnet)
