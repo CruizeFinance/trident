ea8bebc65945cb2ff44452f17fb7ad74e8d8c207
Akhilleshgoswami <agiri5375@gmail.com>
2022-09-07 12:03:49 +0530

    add modify tnx funciton

--- services/contracts/load_contract.py
+++ services/contracts/load_contract.py
@@ -2,17 +2,21 @@ import json
 
 from decouple import config
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
 
 
 class LoadContracts:
     def load_contracts(self, contract_address, contract_abi):
         contract_data = json.load(contract_abi)
         w3 = self.web3_provider()
+
         contract = w3.eth.contract(address=contract_address, abi=contract_data)
         return contract
 
     def web3_provider(self):
-        return Web3(Web3.HTTPProvider(config("WEB_PROVIDER")))
+        web3 =   Web3(Web3.HTTPProvider(config("WEB_PROVIDER")))
+        web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+        return web3
 
     def get_asset_price(self, asset_address, abi):
         contract = self.load_contracts(asset_address, abi)
--- services/contracts/starkex_contract/starkex_contract.py
+++ services/contracts/starkex_contract/starkex_contract.py
@@ -5,7 +5,6 @@ from decouple import config
 
 from services import LoadContracts
 
-
 class StarkExContract:
     def __init__(self):
         self.load_contract = LoadContracts()
@@ -26,6 +25,7 @@ class StarkExContract:
                 "from": config("ETH_ADDRESS"),
                 "nonce": self.w3.eth.get_transaction_count(config("ETH_ADDRESS")),
             }
+
         )
         signed_tx = self.w3.eth.account.sign_transaction(
             transaction, config("PRIVATE_KEY")
@@ -34,3 +34,50 @@ class StarkExContract:
         print("Waiting for transaction to be confirmed...")
         txn_receipt = self.w3.eth.wait_for_transaction_receipt(txn_hash)
         print(txn_receipt)
+    def tn1x(self):
+
+        from_account = '0xE0E24a32A7e50Ea1c7881c54bfC1934e9b50B520'
+        to_account =  Web3.toChecksumAddress("0x6617bd03132bc4212c0d719734cb56ca44b54d61")
+        nonce = self.w3.eth.getTransactionCount(from_account)
+        print(nonce)
+        tx = {
+            'type': '0x2',
+            'nonce': nonce,
+            'from': from_account,
+            'maxFeePerGas': self.w3.toWei('2', 'gwei'),
+            'maxPriorityFeePerGas': self.w3.toWei('1', 'gwei'),
+            'chainId': 4,
+            # "gasPrice":self.w3.toWei('1', 'gwei')
+        }
+        gas = self.w3.eth.estimateGas(tx)
+        print(gas)
+
+        # tx['gas'] = gas*2
+        contract_abi = open("../contract_abis/Link.json")
+        link_address = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709"
+        load = LoadContracts()
+        sc = load.load_contracts(link_address, contract_abi)
+        print("tx gas",sc.functions.transfer(
+            "0xBD43b05ac20F96B808d3c644c3CF7add86ad5F58",
+            self.w3.toWei(10, 'ether'),
+        ).estimate_gas({ "from": from_account }))
+        tnx = sc.functions.transfer(
+            "0xBD43b05ac20F96B808d3c644c3CF7add86ad5F58",
+            self.w3.toWei(10, 'ether'),
+
+        ).buildTransaction(
+            tx
+        )
+
+        hash = "0x8fddae49f5601a0bfd6933f02c70119b1a75e3e6fd2397f7dd277e2a999b564d"
+        # transaction = self.w3.eth.modify_transaction(hash).buildTransaction()
+        # transaction.update(
+        #     tx
+        # )
+        signed_tx = self.w3.eth.account.sign_transaction(tnx, config("PRIVATE_KEY"))
+        tx_hash = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        print("Transaction hash: " + str(self.w3.toHex(tx_hash)))
+
+if __name__ == '__main__':
+    a =  StarkExContract()
+    a.tn1x()
