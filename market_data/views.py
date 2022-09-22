b6d101d934af818d074c524aeb0e7948ba21b316
Akhilleshgoswami <agiri5375@gmail.com>
2022-09-22 13:57:34 +0530

    add allowed host

--- market_data/views.py
+++ market_data/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,6 +15,7 @@ from services.market_data import coingecko
 
 
 class MarketData(GenericViewSet):
+
     def market_chart_day(self, request):
         result = {"prices": None, "error": None}
         self.serializer_class = MarketDataDayRequestSerializer
