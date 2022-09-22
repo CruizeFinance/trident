e38be6bd1e9e7dc9b39f1829b69e37d3f9cefa53
Prithviraj8 <prithvimurthy8@gmail.com>
2022-09-22 12:33:09 +0530

    add asset price api

--- market_data/views.py
+++ market_data/views.py
@@ -8,6 +8,7 @@ from rest_framework.viewsets import GenericViewSet
 from market_data.serializers import (
     MarketDataDayRequestSerializer,
     MarketDataTimestampRequestSerializer,
+    AssetPriceRequestSerializer,
 )
 from services.market_data import coingecko
 
@@ -37,6 +38,20 @@ class MarketData(GenericViewSet):
         try:
             market_chart_day_result = coingecko.market_chart_timestamp(**data)
             result["prices"] = market_chart_day_result["prices"]
+            return Response(data=result, status=status.HTTP_200_OK)
+        except Exception as e:
+            result["error"] = str(e)
+            return Response(data=result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def asset_price(self, request):
+        result = {"price": None, "error": None}
+        self.serializer_class = AssetPriceRequestSerializer
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.data
+        try:
+            asset_price = coingecko.asset_price(**data)
+            result["price"] = asset_price
             return Response(data=result, status=status.HTTP_200_OK)
         except Exception as e:
             result["error"] = str(e)
