7c0d7696bc7c98125b1e0f5e0935385cff9644bf
salmanbao <salmancodez@gmail.com>
2022-11-04 12:33:43 +0500

    add requirements.txt in stop.sh file

--- deployment_scripts/install_requirements.sh
+++ deployment_scripts/install_requirements.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo apt-get install -y python-psycopg2 libncurses5-dev libffi libffi-devel libxml2-devel libxslt-devel libxslt1-dev
+sudo apt-get install -y python-lxml python-devel gcc patch python-setuptools
+sudo apt-get install -y gcc-c++ flex epel-release
+
 sudo pip3 install virtualenv
 virtualenv env
 source env/bin/activate
