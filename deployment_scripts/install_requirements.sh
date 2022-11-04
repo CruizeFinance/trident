d452dfb0d7456964bdb487912d709f3b7c927bf6
salmanbao <salmancodez@gmail.com>
2022-11-04 12:51:58 +0500

    add requirements.txt in stop.sh file

--- deployment_scripts/install_requirements.sh
+++ deployment_scripts/install_requirements.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+rm -rf /home/ubuntu/trident
 sudo apt-get install python3-pip python3-dev nginx
 sudo apt-get install -y python-psycopg2 libncurses5-dev libffi libffi-devel libxml2-devel libxslt-devel libxslt1-dev
 sudo apt-get install -y python-lxml python-devel gcc patch python-setuptools
