98b301867a5e354c0cbe884982cee10795eafba6
salmanbao <salmancodez@gmail.com>
2022-11-04 12:15:12 +0500

    add requirements.txt in stop.sh file

--- deployment_scripts/install_requirements.sh
+++ deployment_scripts/install_requirements.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
- pip install -r ../requirements.txt
+sudo pip3 install virtualenv
+virtualenv env
+source env/bin/activate
+echo $PWD
+req="/../requirements.txt"
+script_dir=$(dirname "$0")
+pip install -r "$script_dir$req
